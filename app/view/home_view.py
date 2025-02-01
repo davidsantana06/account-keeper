153193fbac8643f29717c9a485185aff6ba3c5d1
David Santana <davidsantana06@hotmail.com>
2025-02-01 11:19:44 -0300

    feat(home): add first view redirect

--- app/view/home_view.py
+++ app/view/home_view.py
@@ -1,9 +1,19 @@
+from flask import session
 from flask_classful import FlaskView
-from app.facade import ResponseFacade
+
+from app.facade import ResponseFacade, URLFacade
+from app.service import UserService
 
 
 class HomeView(FlaskView):
     route_base = "/"
+
+    def before_request(self, _: str):
+        is_first_acess = "first_acess" not in session
+        if is_first_acess:
+            session["first_acess"] = False
+            user = UserService.get()
+            return ResponseFacade.as_sync_redirect(URLFacade.for_view(user.first_view))
 
     def index(self):
         return ResponseFacade.as_page("home:index")
