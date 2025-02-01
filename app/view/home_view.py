526a46585f4faf547b71b5b4ce8a79aa5c01fa54
David Santana <davidsantana06@hotmail.com>
2025-01-31 21:52:16 -0300

    feat(home): add index page

--- app/view/home_view.py
+++ app/view/home_view.py
@@ -1,8 +1,9 @@
 from flask_classful import FlaskView
+from app.facade import ResponseFacade
 
 
 class HomeView(FlaskView):
-    route_base = '/'
+    route_base = "/"
 
     def index(self):
-        return '...'
+        return ResponseFacade.as_page("home:index")
