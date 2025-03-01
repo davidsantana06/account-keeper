df906a5a607abf52f89de0eab727b455e7fbc6fc
David Santana <davidsantana06@hotmail.com>
2025-03-01 18:53:28 -0300

    feat(user): add update views

--- app/view/user_view.py
+++ app/view/user_view.py
@@ -1,5 +1,9 @@
+from flask import request
 from flask_classful import FlaskView
-from app.facade import ResponseFacade
+
+from app.facade import FlashFacade, ResponseFacade, URLFacade
+from app.form import UserForm
+from app.service import UserService
 
 
 class UserView(FlaskView):
@@ -7,4 +11,12 @@ class UserView(FlaskView):
         return ResponseFacade.as_page("user:index")
 
     def update(self):
-        return ""
+        form = UserForm()
+        UserService.fill(form)
+        return ResponseFacade.as_page("user:update", {"form": form})
+
+    def put(self):
+        form = UserForm(request.form)
+        UserService.update(form)
+        FlashFacade.append("O perfil foi atualizado", "info")
+        return ResponseFacade.as_async_redirect(URLFacade.for_view("user:index"))
