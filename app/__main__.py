1e95620231927cec8a5c629d78917bc5bbb5e919
David Santana <davidsantana06@hotmail.com>
2025-06-07 18:55:44 -0300

    feat: add cross-platform support

--- app/__main__.py
+++ app/__main__.py
@@ -1,7 +1,18 @@
+import platform
 import webview
 from . import app
 
 
-if __name__ == "__main__":
+def run_in_windows():
     webview.create_window("Account Keeper", app, min_size=(1024, 768))
     webview.start()
+
+
+def run_in_other_systems():
+    app.run()
+
+
+if __name__ == "__main__":
+    is_windows = platform.system() == "Windows"
+    runner = run_in_windows if is_windows else run_in_other_systems
+    runner()
