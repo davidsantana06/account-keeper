a6713e23631977e64396d462dc72103c55ac8294
David Santana <davidsantana06@hotmail.com>
2025-06-20 10:46:40 -0300

    refactor: remove `resource/` folder

--- app/config/path.py
+++ app/config/path.py
@@ -8,11 +8,8 @@ class Path:
     DATABASE_FILE = path.join(ROOT_FOLDER, "db.sqlite3")
     """ /db.sqlite3 """
 
-    RESOURCE_FOLDER = path.join(ROOT_FOLDER, "resource")
-    """ /resource """
+    STATIC_FOLDER = path.join(ROOT_FOLDER, "static")
+    """ /static """
 
-    STATIC_FOLDER = path.join(RESOURCE_FOLDER, "static")
-    """ /resource/static """
-
-    TEMPLATE_FOLDER = path.join(RESOURCE_FOLDER, "template")
-    """ /resource/template """
+    TEMPLATE_FOLDER = path.join(ROOT_FOLDER, "template")
+    """ /template """
